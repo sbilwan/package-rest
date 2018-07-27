497ff31021d4ffede7acf2fae4814476e0969eda
sagar <sag@telensa.com>
2018-07-27 21:30:38 +0100

    Added basic spring boot features.

---
+++ src/main/java/com/exercise/rest/PackageRestApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackageRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PackageRestApplication.class, args);
+    }
+}
